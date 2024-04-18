4a48f16d8c936718c310f1aeca09d1c6d4046eab
alex <alex.m.elhage@gmail.com>
2024-04-18 13:46:47 -0400

    spruce up front page ui

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,10 +1,76 @@
-<nav>
-  <a href="/">Home</a>
-  <a href="/schedule">Schedule</a>
-  <a href="/travel">Travel</a>
-  <a href="/rsvp">RSVP</a>
-  <a href="/registry">Registry</a>
-  <a href="/moments">Moments</a>
-</nav>
+<script>
+  import { page } from "$app/stores";
+</script>
 
-<slot />
+<div class="app-root">
+  <div class="app-header">
+    <span>A & L</span>
+    <nav>
+      <a href="/" class:active={$page.url.pathname === "/"}>Home</a>
+      <a href="/schedule" class:active={$page.url.pathname === "/schedule"}
+        >Schedule</a
+      >
+      <a href="/travel" class:active={$page.url.pathname === "/travel"}
+        >Travel</a
+      >
+      <a href="/rsvp" class:active={$page.url.pathname === "/rsvp"}>RSVP</a>
+      <a href="/registry" class:active={$page.url.pathname === "/registry"}
+        >Registry</a
+      >
+      <a href="/moments" class:active={$page.url.pathname === "/moments"}
+        >Moments</a
+      >
+    </nav>
+  </div>
+
+  <slot />
+</div>
+
+<style>
+  :root {
+    --gold: #ab8d6c;
+    --emerald: #24341f;
+    --amethyst: #39023c;
+    --ruby: #610629;
+    --sapphire: #0d0e38;
+  }
+
+  :global(body) {
+    margin: auto;
+    padding: 0;
+    width: 100vh;
+    min-height: 100vh;
+    color: var(--gold);
+    background-color: var(--emerald);
+  }
+
+  .app-root {
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .app-header {
+    margin: 16px;
+  }
+
+  .app-header > nav {
+    display: inline;
+    float: right;
+  }
+
+  .app-header > nav > * {
+    margin: 0 4px;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--gold);
+  }
+
+  .active {
+    text-decoration: underline;
+    text-underline-offset: 8px;
+  }
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import cover from "$lib/assets/cover.jpg";
+</script>
+
+<div class="root">
+  <div class="cover-container">
+    <img class="cover-image" alt="" src={cover} />
+    <div class="cover-text">
+      <div class="cover-names">
+        <div>ALEX</div>
+        <div>&</div>
+        <div>LYNN</div>
+      </div>
+      <div class="cover-date">October 11th, 2024 | Buffalo, NY</div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .cover-container {
+    width: 100%;
+  }
+
+  .cover-image {
+    width: 100%;
+  }
+
+  .cover-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .cover-names {
+    font-size: xx-large;
+  }
+</style>
